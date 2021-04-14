e942d8cbf3ebe71c379e6877c0ddcaf7fcbe4304
ngombert <gombert.n@gmail.com>
2021-04-14 09:57:23 +0200

    activation de la redirection : stdout et stderr vers les fichiers de logs

--- backup.sh
+++ backup.sh
@@ -55,9 +55,9 @@ then
 fi
 
 # log automatique vers les fichiers
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-#exec 1>${LOCAL_LOGS_BACKUP_DIR}backup.log 2>${LOCAL_LOGS_BACKUP_DIR}error.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>${LOCAL_LOGS_BACKUP_DIR}backup.log 2>${LOCAL_LOGS_BACKUP_DIR}error.log
 
 
 #####
