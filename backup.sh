91a87f7c544a917f4c622d0757b1d08edf9f6376
ngombert <gombert.n@gmail.com>
2021-04-14 09:47:47 +0200

    - ajout de l'auth ssh par clef - ajout gitignore pour retirer le répertoire sandbox du repo - ajout des clefs privées et publiques - ajout de la purge des vieux backup - purge des vieux backup seulement si le rsync réussi

--- backup.sh
+++ backup.sh
@@ -2,10 +2,10 @@
 ##########
 # BACKUP DE SERVEUR LINUX (LAMP)
 #
-# Fichiers : directement en remote vers le serveur
+# Fichiers : copie local pendant qque jours + copie distante
 # Mysql : copie local pendant qque jours + copie distante
-# l'arborescence à destination : /home/user/hostname/date-heure/mysql|fichiers|logs
-# exemple : /home/happy-garden/preprod.happy-garden.es/20210412-1634/
+# l'arborescence à destination : .../hostname/date/mysql|fichiers|logs
+# exemple : /home/happy-garden/preprod.happy-garden.es/20210412/
 #
 # TODO côté serveur de backup : création d'un user par client + ajout de la clé ssh.
 #
@@ -22,8 +22,9 @@
 
 
 
-DATE=$(date +"%Y%m%d")
+DATE=$(date +"%Y-%m-%d")
 HOUR=$(date +"%H%M")
+HOSTNAME=$(hostname)
 
 
 if [[ -f backup.conf ]]
@@ -53,40 +54,75 @@ then
     mkdir -p $LOCAL_LOGS_BACKUP_DIR
 fi
 
+# log automatique vers les fichiers
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>${LOCAL_LOGS_BACKUP_DIR}backup.log 2>${LOCAL_LOGS_BACKUP_DIR}error.log
+
 
 #####
 # FILE BACKUP
 #####
 
-function file_backup() {
-    ARCHIVE_DIRECTORY="$1/$NOW"
-    FILE_LIST="$2"
+function local_file_backup() {
+    BACKUP_DIR=$1
+    FILE_LIST=$2
+    RETENTION=$3
 
-    if [[ ! -d $ARCHIVE_DIRECTORY ]]
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** backing up files to $BACKUP_DIR ****"
+
+    STATUS=0
+    for SOURCE in $(cat $FILE_LIST)
+    do
+        echo "$(date +"%Y%m%d-%H:%M:%S") - copying : $SOURCE"
+        rsync -a $SOURCE $BACKUP_DIR
+        $STATUS=$?
+    done
+
+    if [ ! $STATUS ]
     then
-        echo "Création du répertoire de sauvegarde : $ARCHIVE_DIRECTORY"
-        mkdir -p $ARCHIVE_DIRECTORY
+        echo "$(date +"%Y%m%d-%H:%M:%S") - local file backup succeed, cleaning old backups"
+        local_purge_old_backup $BACKUP_DIR $RETENTION
+    else
+        echo "$(date +"%Y%m%d-%H:%M:%S") - local file backup failed, skipping local file purge"
     fi
 
-    echo "backing up files to $ARCHIVE_FILENAME ..."
-    # TODO : install et test rdiff-backup
-    rdiff-backup --include-globbing-filelist "$FILE_LIST" --exclude '**' / "$ARCHIVE_DIRECTORY"
-
-    echo "end of file backup."
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** end of local file backup ****"
 }
 
-function purge_local_old_backup() {
-    ARCHIVE_DIRECTORY = $1
-    RETENTION_DAYS = $2
+function remote_file_backup() {
+    REMOTE_SRV_USER=$1
+    REMOTE_SRV_NAME=$2
+    REMOTE_SRV_KEY_FILE=$3
+    REMOTE_SRV_DIR=$4
+    BACKUP_DIR=$5
+    RETENTION=$6
 
-    # TODO : 
-    rdiff-backup --remove-older-than "$RETENTION_DAYS"D --force "$ARCHIVE_DIRECTORY"
-}
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** Starting remote file backup ****"
+    if [ ! -f $REMOTE_SRV_KEY_FILE ]
+    then
+        >&2 echo "$(date +"%Y%m%d-%H:%M:%S") - ERROR : rsync/ssh key file not found"
+        exit 1
+    fi
 
-function purge_remote_old_backup() {
-    # rdiff en remote
-    echo "todo"
-    # TODO : 
+    # Create directories in remote if needed
+    if ! ssh -i $REMOTE_SRV_KEY_FILE $REMOTE_SRV_USER@$REMOTE_SRV_NAME "test -d ${REMOTE_SRV_DIR}"; then
+        ssh -i $REMOTE_SRV_KEY_FILE $REMOTE_SRV_USER@$REMOTE_SRV_NAME "mkdir -p ${REMOTE_SRV_DIR}"
+    fi
+
+    echo "$(date +"%Y%m%d-%H:%M:%S") - rsync start"
+    rsync -av -e "ssh -i $REMOTE_SRV_KEY_FILE" $BACKUP_DIR $REMOTE_SRV_USER@$REMOTE_SRV_NAME:$REMOTE_SRV_DIR
+    echo "$(date +"%Y%m%d-%H:%M:%S") - rsync end"
+
+    if [ ! $? ]
+    then
+        echo "$(date +"%Y%m%d-%H:%M:%S") - remote file backup succeed, cleaning old backups"
+        remote_purge_old_backup $REMOTE_SRV_USER $REMOTE_SRV_NAME $REMOTE_SRV_KEY_FILE $REMOTE_SRV_DIR $RETENTION
+    else
+        echo "$(date +"%Y%m%d-%H:%M:%S") - remote file backup failed, skipping old backup purge"
+    fi
+
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** end of remote file backup ****"
 }
 
 #####
@@ -98,12 +134,11 @@ function mysql_database_backup() {
     DB_USER=$2
     DB_PASSWORD=$3
     BACKUP_FILE=$4
-    LOG_FILE=$5
-    DB_NAME=$6
+    DB_NAME=$5
     MYSQL_OPTIONS="--dump-date --no-autocommit --single-transaction --hex-blob --triggers -R -E"
 
-    echo "backing up database $DB_NAME from host $DB_SRV ..."
-    mysqldump -u"$DB_USER" -p"$DB_PASSWORD" $MYSQL_OPTIONS "$DB_NAME" | gzip > $BACKUP_FILE 2> $LOG_FILE
+    echo "$(date +"%Y%m%d-%H:%M:%S") - backing up database $DB_NAME from host $DB_SRV ..."
+    mysqldump -u"$DB_USER" -p"$DB_PASSWORD" $MYSQL_OPTIONS "$DB_NAME" | gzip > $BACKUP_FILE
 }
 
 function mysql_backup_all() {
@@ -113,36 +148,80 @@ function mysql_backup_all() {
     DB_USER=$2
     DB_PASSWORD=$3
     BACKUP_DIR=$4
-    LOG_DIR=$5
+    RETENTION=$5
 
-    echo "**** start of database backup. ****"
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** start of database backup. ****"
 
     DB_LIST=$(mysqlshow | grep -v "+\|performance_schema\|Databases" | cut -d" " -f2)
 
+    STATUS=0
     for DB_NAME in $DB_LIST
     do
         mysql_database_backup $DB_SRV $DB_USER $DB_PASSWORD \
         "${BACKUP_DIR}${DATE}-${HOUR}-${DB_NAME}.sql.gz" \
-        "${LOG_DIR}${DATE}-${HOUR}-mysql-${DB_NAME}-error.log" \
         $DB_NAME
+        $STATUS=$?
     done
 
-    echo "**** end of database backup. ****"
+    # if no error during backup then remove old ones
+    if [! $STATUS ]
+    then
+        local_purge_old_backup $BACKUP_DIR $RETENTION
+    fi
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** end of database backup. ****"
 
 }
 
-function mysql_purge_old_backup() {
-    $BACKUP_DIR=$1
-    $RETENTION=$2
+#####
+# COMMUN
+#####
 
-    #TODO : find -exec +rm
+function local_purge_old_backup() {
+    BACKUP_DIR=$1
+    RETENTION=$2
+
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** cleaning old backup (>$RETENTION days) in $BACKUP_DIR ****"
+    if [ -z $BACKUP_DIR ]
+    then
+        >&2 echo "$(date +"%Y%m%d-%H:%M:%S") - ERROR : $BACKUP_DIR non défini ! on risque de vider le disque !!!"
+        exit 1
+    else
+        find $BACKUP_DIR -mtime +${RETENTION} -exec rm -rf {} \;
+    fi
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** end of backup cleaning ****"
 }
 
+function remote_purge_old_backup() {
+    REMOTE_SRV_USER=$1
+    REMOTE_SRV_NAME=$2
+    REMOTE_SRV_KEY_FILE=$3
+    REMOTE_SRV_DIR=$4
+    RETENTION=$5
+
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** cleaning old backup (>$RETENTION days) in $REMOTE_SRV_DIR on $REMOTE_SRV_NAME ****"
+    if [ -z $REMOTE_SRV_DIR ]
+    then
+        >&2 echo "$(date +"%Y%m%d-%H:%M:%S") - ERROR : $REMOTE_SRV_DIR non défini ! on risque de vider le disque !!!"
+        exit 1
+    else
+        ssh -i $REMOTE_SRV_KEY_FILE $REMOTE_SRV_USER@$REMOTE_SRV_NAME "find $REMOTE_SRV_DIR -mtime +${RETENTION} -exec rm -rf {} \;"
+    fi
+    echo "$(date +"%Y%m%d-%H:%M:%S") - **** end of backup cleaning ****"
+}
+
+
 #####
-# Main
+# MAIN
 #####
 
-mysql_backup_all $DB_SRV_NAME $DB_USER $DB_PASSWORD $LOCAL_MYSQL_BACKUP_DIR $LOCAL_LOGS_BACKUP_DIR
-#file_backup
-#purge_local_old_backup
-#purge_remote_old_backup
+echo "========================================================================="
+echo "= $(date +"%Y%m%d-%H:%M:%S") - BACKUP START"
+echo "========================================================================="
+
+mysql_backup_all $DB_SRV_NAME $DB_USER $DB_PASSWORD $LOCAL_MYSQL_BACKUP_DIR $LOCAL_BACKUP_RETENTION
+local_file_backup $LOCAL_FILE_BACKUP_DIR $FILE_LIST $RETENTION
+remote_file_backup $REMOTE_SRV_USER $REMOTE_SRV_NAME $REMOTE_SRV_KEY_FILE $REMOTE_SRV_DIR $LOCAL_BACKUP_DIR $REMOTE_SRV_RETENTION
+
+echo "========================================================================="
+echo "= $(date +"%Y%m%d-%H:%M:%S") - BACKUP END"
+echo "========================================================================="
